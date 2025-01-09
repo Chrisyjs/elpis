2a1622b060b949a7bf73b7efd4737171a7e81623
罗鹏 <l>
2025-01-09 18:25:56 +0800

    feat: 书写middlewareLoader和routerSchemaLoader

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const Koa = require('koa');
+// 引入elpis-core
+const ElpisCore = require('./elpis-core')
 
-//创建koa实例
-
-const app = new Koa();
-
-//启动服务
-
-const port = process.env.PORT || 8080;
-const host = process.env.IP || '0.0.0.0'
-
-app.listen(port,host);
-
-console.log(`服务已启动,端口号：${port}`)
+ElpisCore.start({
+    name:"Elpis"
+});
